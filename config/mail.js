291194c992c1ca270aa0c72a89e3e7d3c14e3482
maharichu30 <rajmaharichu30@gmail.com>
2026-03-25 05:27:44 -0600

    fixed gmail timeout

--- config/mail.js
+++ config/mail.js
@@ -1,13 +1,27 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
-  }
+  },
+
+  tls: {
+    rejectUnauthorized: false
+  },
+
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000
+
 });
 
 export default transporter;
