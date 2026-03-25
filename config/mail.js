686bbb6a4f23fd6147adb7cff41044325c4943dd
maharichu30 <rajmaharichu30@gmail.com>
2026-03-25 05:40:45 -0600

    completed

--- config/mail.js
+++ config/mail.js
@@ -1,27 +1,13 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
-  },
-
-  tls: {
-    rejectUnauthorized: false
-  },
-
-  connectionTimeout: 20000,
-  greetingTimeout: 20000,
-  socketTimeout: 20000
-
+  }
 });
 
 export default transporter;
